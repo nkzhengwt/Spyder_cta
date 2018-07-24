b7bbda85ba8b75b2660d8ff72abdc071e7328f97
Wentao Zheng <x1019575585@163.com>
2018-07-24 16:37:42 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -16,10 +16,9 @@ warnings.filterwarnings("ignore")
 
 if __name__=='__main__':
     # 设置主路径
-    homePath = 'E:\\Intern\\zxjt\\test10'
-    # 设置持仓排名参数，分别代表前n名持仓排名
-#    params = [5,10,20]
-    params =[20] # 目前参数功能尚未补全，故只取20作为参数
+    homePath = 'E:\\Intern\\zxjt\\test14'
+    # 设置持仓排名参数，代表前n名持仓排名
+    params =[20] #参数选择从5,10,20中选择
     # 设置保证金比率
     deposit = 1#0.15
     # 设置佣金比率
@@ -28,21 +27,19 @@ if __name__=='__main__':
     multiplier = 300
     # 设置起始日期
     updatebegin = 20100416
-    endDate = 20180601
+    endDate = 20180724
     # 设置合约品种
     windSymbol = 'IF.CFE'
     # 提取信号
-#    IF = oir(homePath,updatebegin,endDate)
-#    temp = IF.get_signal_cffex(windSymbol)
-#    sig = temp[['tradeDate','signal']]
-#    sig.columns = ['tradeDate','signal20']
-#    sig.to_hdf('rank.h5','signal'+windSymbol)
-    sig = pd.read_hdf('rank.h5','signal'+windSymbol)
+    IF = oir(homePath,updatebegin,endDate)
+    sig = IF.get_signal_cffex(windSymbol)
+    sig.to_hdf('rank.h5','signal'+windSymbol)
+#    sig = pd.read_hdf('rank.h5','signal'+windSymbol)
     # 提取或更新合约行情数据
-#    Ob_quo = Quo(homePath,updatebegin ,endDate)
-#    quo = Ob_quo.updateDataFromWind(windSymbol)
-#    quo.to_hdf('quo.h5',windSymbol)
-    quo = pd.read_hdf('quo.h5',windSymbol)
+    Ob_quo = Quo(homePath,updatebegin ,endDate)
+    quo = Ob_quo.updateDataFromWind(windSymbol)
+    quo.to_hdf('quo.h5',windSymbol)
+#    quo = pd.read_hdf('quo.h5',windSymbol)
     # 模拟下单，打印交易日志，生成资金曲线
     Ob_asset = assetcurve(sig,quo,params,deposit,commission)
     asset = Ob_asset.get_curve()
@@ -50,4 +47,4 @@ if __name__=='__main__':
     # 根据资金曲线计算回测指标，记录回测结果，绘图
     indicators = Indicators(asset,params)
     indicators.write_indicators_concat(homePath+'\\write_indicators.csv')
-    indicators.plot_figure(20)
+    indicators.plot_figure(params[0])
